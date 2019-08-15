1501ec61c182d5c11becfd947d5d9f8e3f75a610
Cid Medeiros <46410491+cidmedeiros@users.noreply.github.com>
2019-08-15 19:10:06 -0300

    Add two prototypes

--- knife-files.py
+++ knife-files.py
@@ -18,5 +18,38 @@ def manual_rename_files():
     for f in os.listdir():
         f_name, f_ext = (os.path.splitext(f))
         new_name = input('New name for file {}:'.format(f_name))
-        os.rename(f, new_name)    
+        os.rename(f, new_name)
+        
+def folha_rosto_proeb(data):
+    for index, bolsista in data.iterrows():
+        entidade = bolsista.nome_entidade_local
+        f_dir = re.sub('/', '_', entidade)
+        f_dir = f_dir.upper()
+        n_sei = ''
+        n_cpf = bolsista.cpf_bolsista
+        b_nome = bolsista.nome_bolsista
+        if not os.path.exists(f_dir):
+            os.makedirs(f_dir)
+        template = 'folha-rosto.docx'
+        document = mg.MailMerge(template)
+        document.merge(ies=entidade, sei=n_sei, cpf=n_cpf, nome=b_nome)
+        document.write('{}/{}.docx'.format(f_dir,bolsista.nome_bolsista))
+
+def lista_ies_proeb(data):
+    ies_list = list(data.nome_entidade_local.unique())
+    for ies in ies_list:
+        os.chdir(r'C:\Users\cidm\Documents\python_control\proeb')
+        f_dir = re.sub('/', '_', ies)
+        f_dir = f_dir.upper()
+        if not os.path.exists(f_dir):
+            os.makedirs(f_dir)
+        os.chdir(r'{}'.format(f_dir))
+        bolsistas_list = data.query('nome_entidade_local == "{}"'.format(ies))
+        if len(ies)> 30:
+            nome_ies = ies[:31]
+            nome_ies = re.sub('/', '_', nome_ies)
+        else:
+            nome_ies = re.sub('/', '_', ies)
+        excel(bolsistas_list, nome_ies)
+
 
