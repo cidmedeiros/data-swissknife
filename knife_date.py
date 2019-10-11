2d6e57cd8fe90caf54c18a0d18088cb1d50329dd
Cid Medeiros <46410491+cidmedeiros@users.noreply.github.com>
2019-10-11 17:34:48 -0300

    add make_date() and make_date_series()

---
+++ knife_date.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 14:15:17 2019
+
+@author: cidmedeiros
+"""
+import datetime as dt
+import pandas as pd
+
+def make_date(year,month,day):
+    
+    """
+    Make the date out of seperated integer values. 
+    """
+    date = dt.date(year = year, month=month, day=day)
+    
+    return date
+
+def make_date_series(series_year, series_month, series_day):
+    
+    """
+    Applies make_date on a non date format pandas series object. Ideal for datasets with dates
+    organized seperated columns for year, month and day, instead of single column for date.
+    series_year: pandas series type int;
+    series_month: pandas series type int;
+    series_day: pandas series type int;
+    
+    """
+    serie = pd.to_datetime((series_year*10000+series_month*100+series_day).apply(str),format='%Y%m%d')
+    
+    serie = [make_date(x.year, x.month, x.day) for x in serie]
+        
+    return serie
+
+class Date:
+    def __init__(self, year, month, day):      
+        self.year = year
+        self.month = month
+        self.day = day
+        
+    def std_same_month(self, end=False):
+        
+        """
+        Standardize a given date at the first or last day of date's month,
+        unless end is set to True.
+        
+        Parameters:
+        date (datetime): date to be stadandized
+        end (boolean):
+            If True returns the date stadandized at the last day of the month.
+            The ending day is 28 for February and 30 for the rest of the months.
+            If False (default) returns the date stadandized at the first day of the month.
+        
+        Returns:
+        datetime: returning date stadandized
+        """
+        ans = None
+        if end == False:
+            ans = dt.datetime(self.year, self.month, 1)
+        else:
+            if self.month != 2:
+                ans = dt.datetime(self.year, self.month, 30)
+            else:
+                ans = dt.datetime(self.year, self.month, 28)
+                
+        return ans
+    
+    def sum_one_month(self, end=False):
+        
+        """
+        Return the next month of the informed date.
+        Standardize the answer at the first day of date, unless end is set to True.
+        
+        Parameters:
+        date (datetime): date to be added to and stadandized
+        end (boolean):
+            If True returns the date stadandized at the last day of the month.
+            The ending day is 28 for February and 30 for the rest of the months.
+            If False (default) returns the date stadandized at the first day of the month;
+        
+        Returns:
+        datetime: returning date stadandized
+        """
+        ans = None
+        
+        if end == False:
+            if self.month != 12:
+                ans = dt.datetime(self.year,((self.month)+1),1)
+            else:
+                ans = dt.datetime((self.year+1), 1, 1)
+        else:
+            if self.month != 12:
+                if self.month != 1:
+                    ans = dt.datetime(self.year, (self.month+1), 30)
+                else:
+                    ans = dt.datetime(self.year, (self.month+1), 28)
+            else:
+                ans = dt.datetime((self.year+1), 1, 30)
+                
+        return ans
+    
+    def sub_one_month(self, end=False):
+        
+        """
+        Return the previous month of the informed date.
+        Standardize the answer at the first day of date, unless end is set to True.
+        	Parameters:
+        	date (datetime): date to be subtracted from and stadandized
+                end (boolean):
+                    If True returns the date stadandized at the last day of the month.
+                    The ending day is 28 for February and 30 for the rest of the months.
+                    If False (default) returns the date stadandized at the first day of the month;
+                
+            Returns:
+            datetime: returning date stadandized  
+        """
+        
+        ans = None
+        if end == False:
+            if self.month != 1:
+                ans = dt.datetime(self.year,((self.month)-1),1)
+            else:
+                ans = dt.datetime((self.year-1), 12, 1)
+        else:
+            if self.month != 1:
+                if self.month != 3:
+                    ans = dt.datetime(self.year, (self.month-1), 30)
+                else:
+                    ans = dt.datetime(self.year, (self.month-1), 28)
+            else:
+                ans = dt.datetime((self.year-1), 12, 30)
+                
+        return ans
+
+    def add_years(self, years):
+        
+        """
+        Return a date that's "years" years after the informed date. Return the
+        same calendar date (month and day) in the destination year, if it exists,
+        otherwise use the following day (thus changing February 29 to March 1).
+        """ 
+        try:
+            return self.replace(year = self.year + years)
+        except ValueError:
+            return self + (dt.date(self.year + years, 1, 1) - dt.date(self.year, 1, 1))
+        
